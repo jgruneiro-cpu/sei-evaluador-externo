ae8d6f4989da9f171205ffaa68d2fc3b14f1403d
jgruneiro-cpu <jgruneiro@gmail.com>
2026-07-01 14:30:21 +0200

    Update sei.js

--- netlify/functions/sei.js
+++ netlify/functions/sei.js
@@ -1,6 +1,26 @@
 exports.handler = async (event) => {
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS'
+      },
+      body: ''
+    };
+  }
+
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  if (!process.env.ANTHROPIC_API_KEY) {
+    return {
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: 'ANTHROPIC_API_KEY no configurada en Netlify' })
+    };
   }
 
   try {
@@ -27,6 +47,7 @@ exports.handler = async (event) => {
       body: JSON.stringify(data)
     };
   } catch (error) {
+    console.error('SEI function error:', error);
     return {
       statusCode: 500,
       headers: { 'Access-Control-Allow-Origin': '*' },
